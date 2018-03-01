ecc8493ad90c25d5cc7faf3cd28a29eab1c7a95a
Pierre Bekker <BekkerP@Aforbes.co.za>
2018-03-01 18:56:50 +0200

    First line of code.

--- HashCode2018/HashCode2018/Program.cs
+++ HashCode2018/HashCode2018/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace HashCode2018
+﻿using System;
+
+namespace HashCode2018
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
